25e4a3f0c6364588d28e9bd46db7824222fbcc87
jo030225 <48784553+jo030225@users.noreply.github.com>
2020-08-23 11:58:40 +0900

    회원가입 알림창 추가
    회원가입 후 화면전환은 아직 제대로 구현하지 못했다.

--- FirebaseTest/SignUpViewController.swift
+++ FirebaseTest/SignUpViewController.swift
@@ -21,14 +21,39 @@ class SignUpViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func backMain(){
+        let loginPage = self.storyboard?.instantiateViewController(withIdentifier: "LoginPage")
+        self.present(loginPage!, animated: true, completion: nil)
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func signUpSuccessAlert(){
+        let alert = UIAlertController(title: "회원가입 성공", message: "회원가입을 성공했습니다", preferredStyle: UIAlertController.Style.alert)
+        let ok = UIAlertAction(title: "확인", style: UIAlertAction.Style.default)
+        alert.addAction(ok)
+        self.present(alert, animated: false)
+    }
+    
+    func signUpFailAlert(){
+           let alert = UIAlertController(title: "회원가입 실패", message: "회원가입을 실패했습니다", preferredStyle: UIAlertController.Style.alert)
+           let ok = UIAlertAction(title: "확인", style: UIAlertAction.Style.default)
+           alert.addAction(ok)
+           self.present(alert, animated: false)
+       }
+    
     @IBAction func signUpDone(_ sender: Any) {
         Auth.auth().createUser(withEmail: signUpEmailTextField.text!, password: signUpPwdTextField.text!
                   ) { (user, error) in
                       if user !=  nil{
-                          print("register success")
+                        print("register success")
+                        self.signUpSuccessAlert()
+                        self.backMain()
                       }
                       else{
-                          print("register failed")
+                        print("register failed")
+                        self.signUpEmailTextField.text = ""
+                        self.signUpPwdTextField.text = ""
+                        self.signUpFailAlert()
                       }
                   }
 
